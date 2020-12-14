7ab4ae6a5c9a44b37b9141d5990b241cf21cc846
Jesal Mehta <jesaldmehta@gmail.com>
2020-12-13 22:16:55 -0500

    Designed a database schema containing the 3 tables - department, role and employee in schema.sql file

---
+++ schema.sql
@@ -0,0 +1,34 @@
+-- deletes database if it already exists
+DROP DATABASE IF EXISTS employee_TrackerDB;
+
+-- creates database
+CREATE DATABASE employee_TrackerDB;
+
+-- creates the following 3 tables in this database
+USE employee_TrackerDB;
+
+-- creates table 'department' with 2 columns
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+  );
+ 
+-- creates table 'role' with 4 columns
+CREATE TABLE role(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE CASCADE
+  );
+
+-- creates table 'employee' with 5 columns
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NOT NULL,
+  FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE,
+  FOREIGN KEY (manager_id) REFERENCES employee (id) ON DELETE CASCADE
+  );
